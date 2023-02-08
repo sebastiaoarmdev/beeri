f5cda30fb858f86090e0210aea1e7e1963bf28ef
Sebastião Moura <97861831+sebastiaoarmdev@users.noreply.github.com>
2023-02-08 18:18:41 -0300

    🚧 implement PWA

---
+++ sw.js
@@ -0,0 +1,34 @@
+const cacheName = 'cache';
+const precacheResources = [
+    "./",
+    "./index.html",
+    "./styles/main.css",
+    "./scripts/main.js",
+    "./images/can.png",
+    "./images/drink.png",
+    "./images/glass.png",
+    "./images/minus.png",
+    "./images/mug.png",
+    "./images/plus.png"
+];
+
+self.addEventListener('install', (event) => {
+    console.log('Service worker install event!');
+    event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service worker activate event!');
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log('Fetch intercepted for:', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        }),
+    );
+});
